340a2ea0e2542b4822efcc35fc86802e12bca141
pudelko <wojciech.pudelko@gmail.com>
2022-10-22 09:34:00 +0200

    first trial commit and minor clean up

--- piva/main.py
+++ piva/main.py
@@ -18,39 +18,11 @@ os.environ['QT_MAC_WANTS_LAYER'] = '1'
 
 
 def db():
-    print('++++++++++++++++++++++++++++++++++++')
-    print('\tdevelopement version')
-    print('++++++++++++++++++++++++++++++++++++')
+    print('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
+    print('\tPIVA - Python Interactive Viewer for Arpes')
+    print('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
     app = QApplication(sys.argv)
     window = DataBrowser()
-    sys.exit(app.exec_())
-
-###################################
-########### deprecated ############
-###################################
-
-
-def open2D():
-    app = QApplication(sys.argv)
-    fname = sys.argv[1]
-    # if len(sys.argv) == 3:
-    #     pickle = True
-    # else:
-    #     pickle = False
-    data_set = dl.load_data(fname)
-    p2d.MainWindow2D("data_browser", data_set=data_set, title=fname)
-    sys.exit(app.exec_())
-
-
-def open3D():
-    app = QApplication(sys.argv)
-    fname = sys.argv[1]
-    # if len(sys.argv) == 3:
-    #     pickle = True
-    # else:
-    #     pickle = False
-    data_set = dl.load_data(fname)
-    p3d.MainWindow3D("data_browser", data_set=data_set, title=fname)
     sys.exit(app.exec_())
 
 
@@ -59,15 +31,6 @@ def pickle_h5():
     data = dl.load_data(fname)
     dl.dump(data, fname[:-3])
 
-
-def open3Djn(fname, pickle=False, ns=None):
-    app = QApplication(sys.argv)
-    p3d.MainWindow3D(fname=fname, from_pickle=pickle, ns=ns)
-    sys.exit(app.exec_())
-
-
-def reshape_pickled():
-    dl.reshape_pickled(sys.argv[1])
 
 if __name__ == "__main__" :
     db()
