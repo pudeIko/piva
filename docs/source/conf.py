41a708c9f210b15b375dc4c4a68693d3fbadad19
pudelko <wojciech.pudelko@gmail.com>
2023-10-24 11:55:02 +0200

    fixing docs building

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,9 +13,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../../'))
-sys.path.insert(0, os.path.abspath('../piva'))
-sys.path.insert(0, os.path.abspath(os.path.join('../..')))
+sys.path.insert(0, os.path.abspath(os.path.join('../../piva')))
+sys.path.insert(0, os.path.abspath(os.path.join('../../')))
 # sys.path.insert(0, os.path.abspath('_ext/'))
 
 
@@ -51,8 +50,6 @@ exclude_patterns = []
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-# html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
